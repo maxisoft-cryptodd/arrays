8da965e097038447fc12b7b54cd782e7e9e3fd95
maxisoft <maxisoft.eu@gmail.com>
2025-10-17 11:02:31 +0200

    feat: Align Buffer alignment with HWY constants (mimalloc)
    Introduce static assertions in `buffer.cpp` to enforce alignment
    consistency with `HWY_ALIGNMENT`.

--- src/data_io/buffer.cpp
+++ src/data_io/buffer.cpp
@@ -1,3 +1,7 @@
 #include "buffer.h"
 
+#include <hwy/aligned_allocator.h>
 // All Buffer methods are defined in the header to allow for inlining.
+
+static_assert(cryptodd::details::_DEFAULT_HWY_ALIGNMENT >= HWY_ALIGNMENT, "_DEFAULT_HWY_ALIGNMENT must remain aligned with HWY_ALIGNMENT (from highway google lib)");
+static_assert(cryptodd::details::_DEFAULT_HWY_ALIGNMENT % HWY_ALIGNMENT == 0);
